59bf68191dae96ee8f37368a3dc314eddcd81784
pratheesh <pratheesh1269@gmail.com>
2021-11-15 10:03:36 +0800

    add js to respect system preferences for theme

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#theme-toggle").addEventListener("change", () => {
+  /* ----------- switch light/dark mode ----------- */
+  // adapted from tailwindcss docs https://tailwindcss.com/docs/dark-mode
+  const toggleTheme = document.querySelector("#theme-toggle");
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.body.classList.add("dark");
+    toggleTheme.checked = true;
+  } else {
+    document.body.classList.remove("dark");
+    toggleTheme.checked = false;
+  }
+
+  localStorage.removeItem("theme");
+  toggleTheme.addEventListener("change", () => {
+    localStorage.theme = "light" ? "dark" : "light";
     document.body.classList.toggle("dark");
   });
+  /* ----------- end of switch light/dark mode ----------- */
 });

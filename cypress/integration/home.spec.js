7d2712f77dd23d5326c51c43a664712e8ad094b4
pratheesh <pratheesh1269@gmail.com>
2021-12-06 21:18:02 +0800

    complete e2e testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -191,3 +191,123 @@ describe("Reset Filter", () => {
       .should("include", "/products/home");
   });
 });
+
+describe("Delete Product", () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit("/products/home");
+    cy.scrollTo(0, 0);
+  });
+
+  it("asks for confirmation before deleting a product", () => {
+    cy.get(".fa-trash-alt").first().click();
+    cy.contains(
+      "Are you sure you want to delete this listing? This action cannot be undone."
+    ).should("be.visible");
+    cy.get("button").contains("Cancel").should("be.visible");
+    cy.contains("Confirm Deletion").should("be.visible");
+  });
+
+  it("cancels deletion", () => {
+    cy.get(".fa-trash-alt").first().click();
+    cy.contains("Cancel").click();
+    cy.contains("Are you sure you want to delete this listing?").should(
+      "not.be.visible"
+    );
+  });
+
+  it("deletes a product", () => {
+    cy.get(".fa-trash-alt").each(($el, index, $list) => {
+      //test 5 products
+      if (index < 5) {
+        cy.get(".fa-trash-alt").eq(index).click();
+        cy.get(".delete-confirmation").should("be.visible").first().click({
+          force: true,
+        });
+        if (cy.get("body").contains("Error")) {
+          cy.contains(
+            "This product has orders associated with it and cannot be deleted."
+          ).should("be.visible");
+        } else {
+          cy.contains(" Product deleted successfully!").should("be.visible");
+        }
+      }
+    });
+  });
+
+  it("prevents unauthorized deletion", () => {
+    cy.fixture("product").as("product");
+
+    //manually try to delete products
+    cy.get("@product").then((product) => {
+      product.productId.length &&
+        product.productId.forEach((id) => {
+          cy.visit(`/products/${id}/delete`);
+          cy.contains("Product deleted successfully!").should("be.visible");
+        });
+
+      product.noAccessProductId.length &&
+        product.noAccessProductId.forEach((id) => {
+          cy.visit(`/products/${id}/delete`);
+          cy.contains(
+            " You do not have permission to delete this product."
+          ).should("be.visible");
+        });
+    });
+  });
+});
+
+describe("Edit Product", () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit("/products/home");
+    cy.scrollTo(0, 0);
+  });
+
+  it("renders the edit product form", () => {
+    cy.get(".fa-edit").first().click();
+    cy.contains("Edit Product").should("be.visible");
+    cy.contains("Cancel").should("be.visible");
+    cy.contains("Update and Continue").should("be.visible");
+    cy.url().should("include", "/edit");
+  });
+
+  it("cancels editing a product", () => {
+    cy.get(".fa-edit").first().click();
+    cy.contains("Cancel").click();
+    cy.contains("Update and Continue").should("not.exist");
+    cy.url().should("not.include", "/edit");
+  });
+
+  //product form is same as create product form
+  //therefore, no need to test it, only testing navigation
+  it("updates a product", () => {
+    cy.get(".fa-edit").first().click();
+    cy.contains("Update and Continue").click();
+    cy.contains("Product edited successfully!").should("be.visible");
+    cy.url().should("include", "/image");
+    cy.contains("Add Images").click({
+      force: true,
+    });
+    cy.url().should("include", "/tag");
+    cy.contains("Done").click({
+      force: true,
+    });
+    cy.url().should("include", "/products/home");
+  });
+
+  it("prevents unauthorized editing", () => {
+    cy.fixture("product").as("product");
+
+    //manually try to edit products
+    cy.get("@product").then((product) => {
+      product.noAccessProductId.length &&
+        product.noAccessProductId.forEach((id) => {
+          cy.visit(`/products/${id}/edit`);
+          cy.contains("You do not have permission to edit this product").should(
+            "be.visible"
+          );
+        });
+    });
+  });
+});
